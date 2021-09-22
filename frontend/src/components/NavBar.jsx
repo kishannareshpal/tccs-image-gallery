7edaa8c72fbdae46c463dc9ae3b32d894bd0f1fb
Kishan Jadav <kishan_jadav@hotmail.com>
2021-09-22 15:24:20 +0100

    style: navbar background blur

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {
     Container,
     Box,
-    AppBar,
+    AppBar as MuiAppBar,
     Toolbar,
     Button,
     styled,
@@ -10,17 +10,24 @@ import {
 } from "@mui/material";
 import AppLogoPNG from "../assets/images/app_logo.png";
 
-const CreateButton = styled(Button)({
-    borderRadius: 500, // setting a very big border-radius, makes the shape appear pill-like
-    background: "#000",
-    color: "#fff",
-    "&:hover": {
-        background: lighten("#000", 0.2)
-    }
+const AppBar = styled(MuiAppBar)({
+    backgroundColor: "rgba(255,255,255,0.72)",
+    backdropFilter: "blur(20px)",
+    WebkitBackdropFilter: "blur(20px)",
+    borderBottom: "1px solid #dedede"
 });
 
+const CreateButton = styled(Button)(({ theme }) => ({
+    borderRadius: 500, // setting a very big border-radius, makes the shape appear pill-like
+    background: theme.palette.primary.main,
+    color: "#fff",
+    "&:hover": {
+        background: lighten(theme.palette.primary.main, 0.2)
+    }
+}));
+
 const NavBar = () => (
-    <AppBar elevation={0} sx={{ backgroundColor: "white" }} position="static">
+    <AppBar elevation={0} position="sticky">
         <Container>
             <Toolbar disableGutters>
                 <img src={AppLogoPNG} alt="App logo" height={48} />
