efadc132ffb0d6ea74960ff93a40248e2358d4b5
Kishan Jadav <kishan_jadav@hotmail.com>
2021-09-24 10:44:39 +0100

    feat: add galleries routes
    feat: fetch all galleries with GET /api/galleries
    
    feat: store a new gallery with POST /api/galleries

---
+++ backend/app/Http/Controllers/GalleryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gallery;
+use Illuminate\Http\Request;
+use Illuminate\Support\facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class GalleryController extends Controller
+{
+    /**
+     * Retrieve all galleries
+     */
+    public function listAll()
+    {
+        $galleries = Gallery::all();
+
+        // Return the galleries
+        $data = [
+            "galleries" => $galleries
+        ];
+        return $this->respondWithSuccess($data);
+    }
+
+    /**
+     * Retrieve a single gallery by it's ID
+     * 
+     * @param int $id gallery id
+     */
+    public function show($id)
+    {
+        $gallery = Gallery::find($id);
+
+        if (!$gallery) {
+            // Not found
+            return $this->respondWithClientFailure(null, "Gallery not found", 404);
+        }
+
+        // Return the gallery
+        $data = [
+            "gallery" => $gallery
+        ];
+        return $this->respondWithSuccess($data);
+    }
+
+    /**
+     * Create a new gallery
+     * 
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+        // Validate
+        try {
+            $this->validate($request, [
+                "title" => "required|string|min:1|max:70",
+                "description" => "string|max:300|nullable"
+            ]);
+        } catch (ValidationException $e) {
+            return $this->respondWithClientFailure($e->errors(), "Please review the gallery details");
+        }
+
+        // Grab the authenticated user
+        $user = Auth::user();
+
+        // Grab the gallery details 
+        $title = $request->input("title");
+        $description = $request->input("description");
+
+        // Save
+        $gallery = new Gallery();
+        $gallery->title = $title;
+        if (isset($description)) {
+            $gallery->description = $description;
+        }
+        $gallery->user_id = $user->id;
+        $gallery->save();
+
+        // Return it
+        $data = [
+            "gallery" => $gallery
+        ];
+        return $this->respondWithSuccess($data);
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,4 +16,11 @@ $router->group(["prefix" => "api"], function () use ($router) {
     $router->post("login", "AuthController@login");
     $router->post("register", "AuthController@register");
     $router->post("logout", "AuthController@logout");
+
+    $router->get("galleries", "GalleryController@listAll");
+    $router->get("galleries/{id}", "GalleryController@show");
+
+    $router->group(["middleware" => "auth"], function () use ($router) {
+        $router->post("galleries", "GalleryController@store");
+    });
 });
