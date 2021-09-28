7d07141c78a04cc281bd9ed877569cf89d3f0f2b
Kishan Jadav <kishan_jadav@hotmail.com>
2021-09-28 23:09:23 +0100

    feat: add gallery deletion

--- backend/app/Http/Controllers/GalleryController.php
+++ backend/app/Http/Controllers/GalleryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Gallery;
+use Aws\S3\S3Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -82,5 +83,62 @@ class GalleryController extends Controller
             "gallery" => $gallery
         ];
         return $this->respondWithSuccess($data);
+    }
+
+
+    /**
+     * Destroy a gallery
+     * 
+     * @param Request $request
+     */
+    public function destroy(Request $request)
+    {
+        // Validate
+        try {
+            $this->validate($request, [
+                "gallery_id" => "required",
+            ]);
+        } catch (ValidationException $e) {
+            return $this->respondWithClientFailure($e->errors(), "Could not delete the gallery");
+        }
+
+        $galleryId = $request->input("gallery_id");
+        $gallery = Gallery::find($galleryId);
+
+        // Check if gallery exists
+        if (!$gallery) {
+            return $this->respondWithClientFailure(null, "Could not find the gallery to delete", 404);
+        }
+
+        // Check if the authenticated user has permission to delete the gallery.
+        // - basically if they are the owner
+        $userId = Auth::user()->id;
+        $isOwner = $gallery->user_id === $userId;
+        if (!$isOwner) {
+            // Not the owner, prevent and 401 out!
+            return $this->respondWithClientFailure(null, "Gallery could not be deleted. Unauthorized", 401);
+        }
+
+        // Delete all of the objects related to this gallery from the AWS S3 Bucket
+        // Name and region of the bucket where the gallery is stored
+        $bucketName = "tccsimagegallery";
+        $bucketRegion = "us-east-2";
+
+        // Path to the directory of the gallery
+        $galleryDirectoryPath = "galleries/$galleryId";
+
+        // Create the client: Credentials are infered from the Environment Variables
+        $s3Client = new S3Client([
+            'version'     => "latest",
+            'region'      => $bucketRegion
+        ]);
+        // Delete the gallery directory
+        $s3Client->deleteMatchingObjects($bucketName, $galleryDirectoryPath);
+
+        // Delete the gallery from the table
+        $gallery->delete();
+
+        // Respond
+        return $this->respondWithSuccess(null, 200);
     }
 }
