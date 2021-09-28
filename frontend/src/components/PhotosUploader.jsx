9a80b763d49d9cbc790ba934df0c2f428f97b16c
Kishan Jadav <kishan_jadav@hotmail.com>
2021-09-28 13:25:02 +0100

    feat: add image uploading, previewing, listing

--- frontend/src/components/PhotosUploader.jsx
+++ frontend/src/components/PhotosUploader.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import {
     Box,
     Grid,
     Typography,
-    Divider,
+    Alert,
     Button,
     Stack,
-    IconButton,
-    styled
+    styled,
+    LinearProgress
 } from "@mui/material";
-import { Close, Upload } from "@mui/icons-material";
+import { Upload } from "@mui/icons-material";
 import { nanoid } from "nanoid";
-import { useForm } from "react-hook-form";
+import pluralize from "pluralize";
+import { toast } from "react-hot-toast";
+import PropTypes from "prop-types";
 import RoundImage from "./RoundImage";
+import useUser from "../hooks/useUser";
 import VerySimpleButton from "./VerySimpleButton";
+import NetworkServices from "../services/network.services";
 
-const HiddenInput = styled("input")({
-    display: "none"
-});
-
-const ImgUploadBox = styled(Box)({
+/**
+ * The container
+ */
+const PhotosUploaderContainer = styled(Box)({
     textAlign: "start",
     marginTop: 28,
     padding: 24,
@@ -28,148 +31,313 @@ const ImgUploadBox = styled(Box)({
     borderRadius: 12
 });
 
-const PhotoUploader = () => {
-    const [selectedPhotoFiles, setSelectedPhotoFiles] = useState([]);
-    const { register, handleSubmit } = useForm();
+/**
+ * An input that is hidden
+ */
+const HiddenInput = styled("input")({
+    display: "none"
+});
 
-    const selectedPhotosCount = () => selectedPhotoFiles.length;
+const PhotoUploader = ({
+    galleryId,
+    onPhotoPreview,
+    maxSizePerFileInBytes
+}) => {
+    const { user } = useUser();
+    const [selectedPhotosFiles, setSelectedPhotosFiles] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isUploading, setIsUploading] = useState(false);
 
-    const removeSelectedPhotoFile = index => {
-        const prev = [...selectedPhotoFiles];
-        prev.splice(index, 1);
-        setSelectedPhotoFiles(prev);
+    /**
+     * Returns the count of the selected photos files
+     * @returns {int} The number of photos that are choosen
+     */
+    const selectedPhotosCount = () => selectedPhotosFiles.length;
+
+    const removeSelectedPhotoFile = useCallback(
+        /**
+         * Remove a single photo from the list of the {@link selectedPhotosFiles}.
+         * @param {int} index The index of the photo that is being removed from the {@link selectedPhotosFiles}.
+         */
+        index => {
+            setErrorMessage("");
+
+            const prev = [...selectedPhotosFiles];
+            prev.splice(index, 1);
+            setSelectedPhotosFiles(prev);
+        },
+        [selectedPhotosFiles]
+    );
+
+    /**
+     * Remove all of the selected photos files.
+     */
+    const clearSelectedPhotosFiles = () => {
+        setErrorMessage("");
+        setSelectedPhotosFiles([]);
     };
 
-    const clearSelectedPhotosFiles = () => setSelectedPhotoFiles([]);
-
-    const onFileInputChange = e => {
-        // Grab the selected image files
+    /**
+     * Handles on input photo file selection change.
+     * @param {UIEvent} e The change event occured in the input
+     */
+    const onPhotosSelect = e => {
+        setErrorMessage("");
         const { files } = e.target;
-        if (files.length === 0) {
-            // No image file selected, do nothing
+        const filesCount = files.length;
+
+        // No photos selected. Do nothing
+        if (filesCount === 0) {
             return;
         }
 
-        // Add to the list of selected images
-        setSelectedPhotoFiles(prev => [...prev, ...files]);
-    };
-
-    const createSelectedPhotosGridItems = () => {
-        const items = [];
-        for (let i = 0; i < selectedPhotoFiles.length; i += 1) {
-            const file = selectedPhotoFiles[i];
-
-            items.push(
-                <Grid key={file.name + nanoid()} textAlign="center" item xs={2}>
-                    <RoundImage
-                        sx={{ border: "2px solid #E6E8EC", cursor: "zoom-in" }}
-                        width="100%"
-                        height={64}
-                        src={URL.createObjectURL(file)}
-                    />
-
-                    <VerySimpleButton
-                        onClick={() => removeSelectedPhotoFile(i)}
-                        sx={{ fontWeight: "400" }}
-                    >
-                        Remove
-                    </VerySimpleButton>
-                </Grid>
+        if (filesCount > 10) {
+            // Selected more than 10 photos. Not allowed
+            setErrorMessage(
+                "Sorry! You are only allowed to upload a maximum of 10 photos at a time"
             );
+        } else {
+            // Check whether each photo size does not exceed the maximum allowed.
+            const allowedFiles = [];
+            const disallowedFileNames = [];
+            for (let i = 0; i < filesCount; i += 1) {
+                const file = files[i];
+                if (file.size < maxSizePerFileInBytes) {
+                    // Allowed. Does not exceed limit
+                    allowedFiles.push(file);
+                } else {
+                    // Exceeds maximum size limit per file
+                    disallowedFileNames.push(file.name);
+                }
+            }
+
+            if (allowedFiles.length < filesCount) {
+                const maxSizeInMB = maxSizePerFileInBytes / 1000000;
+                setErrorMessage(
+                    <>
+                        {maxSizeInMB} MB size limit per file exceeded for:
+                        {disallowedFileNames.map(name => (
+                            <div>
+                                - <u>{name}</u>
+                            </div>
+                        ))}
+                    </>
+                );
+            }
+            setSelectedPhotosFiles(allowedFiles);
         }
-        return items;
     };
 
-    const onPhotosSubmit = data => {
-        console.log("post files", data);
-    };
+    const createSelectedPhotosPreviewItems = useCallback(
+        /**
+         * Loop through the {@link selectedPhotosFiles} and return MuiGrid items for Previewing selected photos.
+         * @returns {JSX.Element} List of (MuiGrid items) to be used in (MuiGrid container)
+         */
+        () => {
+            const items = [];
+            for (let i = 0; i < selectedPhotosFiles.length; i += 1) {
+                const file = selectedPhotosFiles[i];
+                const tempImageURL = URL.createObjectURL(file);
+
+                items.push(
+                    <Grid
+                        key={file.name + nanoid()}
+                        textAlign="center"
+                        item
+                        xs={2}
+                    >
+                        <RoundImage
+                            sx={{
+                                border: "2px solid #E6E8EC",
+                                cursor: "zoom-in"
+                            }}
+                            onClick={() => onPhotoPreview(tempImageURL)}
+                            width="100%"
+                            height={92}
+                            src={tempImageURL}
+                        />
+
+                        <VerySimpleButton
+                            disabled={isUploading}
+                            onClick={() => removeSelectedPhotoFile(i)}
+                            sx={{ fontWeight: "400" }}
+                        >
+                            Remove
+                        </VerySimpleButton>
+                    </Grid>
+                );
+            }
+            return items;
+        },
+        [
+            onPhotoPreview,
+            removeSelectedPhotoFile,
+            selectedPhotosFiles,
+            isUploading
+        ]
+    );
+
+    const onPhotosSubmit = useCallback(
+        /**
+         * Handle selected photos files submission
+         */
+        async () => {
+            // Reset error messages
+            setErrorMessage(null);
+            // Notify that upload started
+            setIsUploading(true);
+
+            // Prepare the photos files to be uploaded
+            const formData = new FormData();
+            for (let i = 0; i < selectedPhotosFiles.length; i += 1) {
+                // Append each selected file
+                const file = selectedPhotosFiles[i];
+                formData.append("photos[]", file);
+            }
+
+            try {
+                // Request upload
+                const { data } = await NetworkServices.postGalleryPhotos(
+                    galleryId,
+                    formData,
+                    user.token
+                );
+
+                if (data.code === 200) {
+                    // Successful upload
+                    toast.success("Succesfully uploaded to the gallery!");
+                    // Clear the previously selected photos.
+                    setSelectedPhotosFiles([]);
+                } else if (data.code === 400) {
+                    // Error on upload
+                    setErrorMessage(data.message);
+                }
+
+                setIsUploading(false);
+            } catch (error) {
+                // The server could not handle selected files
+                setErrorMessage(
+                    "Could not upload the selected photos. Please try again"
+                );
+                setIsUploading(false);
+            }
+        },
+        [galleryId, selectedPhotosFiles, user.token]
+    );
 
     return (
-        <ImgUploadBox>
-            <Grid
-                container
-                spacing={1}
-                alignItems="center"
-                justifyContent="space-between"
-            >
-                <Grid item xs>
-                    <Typography variant="h6" fontWeight={600}>
-                        Upload photos to this gallery
-                    </Typography>
-                    {selectedPhotoFiles.length > 0 && (
-                        <Box>
-                            <Typography
-                                paragraph
-                                mr={2}
-                                fontWeight={400}
-                                component="span"
-                            >
-                                {selectedPhotosCount()} photos selected
-                            </Typography>
+        <PhotosUploaderContainer>
+            {isUploading && <LinearProgress />}
+            <Grid container sx={{ mt: 2 }}>
+                <Grid
+                    item
+                    container
+                    xs={12}
+                    spacing={1}
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Grid item xs>
+                        <Typography variant="h6" fontWeight={600}>
+                            {!isUploading
+                                ? "Upload photos to this gallery"
+                                : "Uploading..."}
+                        </Typography>
+                        {selectedPhotosFiles.length > 0 && (
+                            <Box>
+                                <Typography
+                                    paragraph
+                                    mr={2}
+                                    fontWeight={400}
+                                    component="span"
+                                >
+                                    {selectedPhotosCount()}/10{" "}
+                                    {pluralize("photo", selectedPhotosCount())}{" "}
+                                    selected
+                                </Typography>
 
-                            <VerySimpleButton
-                                onClick={clearSelectedPhotosFiles}
-                            >
-                                Clear
-                            </VerySimpleButton>
-                        </Box>
-                    )}
-                </Grid>
-                <Grid item xs={12} sm="auto">
-                    <form onSubmit={handleSubmit(onPhotosSubmit)}>
+                                <VerySimpleButton
+                                    disabled={isUploading}
+                                    onClick={clearSelectedPhotosFiles}
+                                >
+                                    Clear
+                                </VerySimpleButton>
+                            </Box>
+                        )}
+                    </Grid>
+                    <Grid item xs={12} sm="auto">
                         <Stack direction="row" spacing={1}>
                             <label htmlFor="upload-image">
                                 <HiddenInput
-                                    accept="image/*"
+                                    accept="image/jpeg, image/png"
                                     id="upload-image"
-                                    {...register("photos")}
+                                    disabled={isUploading}
                                     multiple
-                                    onChange={onFileInputChange}
+                                    onChange={onPhotosSelect}
                                     type="file"
                                 />
                                 <Button
                                     startIcon={<Upload />}
+                                    disabled={isUploading}
+                                    component="span"
                                     variant={
                                         selectedPhotosCount() === 0
                                             ? "black"
                                             : "light"
                                     }
-                                    component="span"
                                 >
                                     {selectedPhotosCount() === 0
                                         ? "Upload"
-                                        : "Upload more"}
+                                        : "Choose others"}
                                 </Button>
                             </label>
 
-                            {selectedPhotoFiles.length > 0 && (
-                                <Button type="submit" variant="black">
+                            {selectedPhotosFiles.length > 0 && (
+                                <Button
+                                    disabled={isUploading}
+                                    onClick={onPhotosSubmit}
+                                    variant="black"
+                                >
                                     Confirm
                                 </Button>
                             )}
                         </Stack>
-                    </form>
+                    </Grid>
+                </Grid>
+
+                <Grid
+                    sx={{
+                        mt: 2,
+                        opacity: isUploading ? 0.5 : 1
+                    }}
+                    item
+                    container
+                    xs={12}
+                    spacing={2}
+                >
+                    {createSelectedPhotosPreviewItems()}
                 </Grid>
             </Grid>
 
-            <Grid container sx={{ mt: 1, mb: 2 }} spacing={2}>
-                {createSelectedPhotosGridItems()}
-            </Grid>
-
-            <Typography variant="subtitle2" fontWeight={400}>
-                Drag and drop image files here, or{" "}
-                <label htmlFor="upload-image">
-                    <strong>
-                        <u>click to browse images</u>
-                    </strong>
-                </label>
-                .
-            </Typography>
-            <Typography variant="subtitle2" color="#bbbbbb" fontWeight={400}>
-                (jpg, png, gif)
-            </Typography>
-        </ImgUploadBox>
+            {errorMessage && (
+                <Alert
+                    onClose={() => setErrorMessage("")}
+                    sx={{ mt: 3 }}
+                    severity="error"
+                >
+                    {errorMessage}
+                </Alert>
+            )}
+        </PhotosUploaderContainer>
     );
+};
+
+PhotoUploader.defaultProps = {
+    maxSizePerFileInBytes: 8000000 // 8 MegaBytes
+};
+
+PhotoUploader.propTypes = {
+    maxSizePerFileInBytes: PropTypes.number
 };
 
 export default PhotoUploader;
--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,14 +1,137 @@
-import React from "react";
-import { Container, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import {
+    Container,
+    Box,
+    Grid,
+    Typography,
+    Divider,
+    Button
+} from "@mui/material";
 
-const Gallery = () => (
-    <Container sx={{ py: 12 }}>
-        <Typography variant="h1" fontWeight={800} marginBottom>
-            My first gallery
-        </Typography>
-        <Typography variant="h5">Kishan Jadav</Typography>
-        <Typography variant="subtitle2">@kishannareshpal</Typography>
-    </Container>
-);
+import { Link, useParams } from "react-router-dom";
+import useUser from "../hooks/useUser";
+import NetworkServices from "../services/network.services";
+import { PhotoCard, PhotosUploader, Lightbox } from "../components";
+
+const Gallery = () => {
+    const { id } = useParams();
+    const { user, isAuthenticated } = useUser();
+    const [gallery, setGallery] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [isPreviewShowing, setIsPreviewShowing] = useState(false);
+    const [previewPhotoURL, setPreviewPhotoURL] = useState("");
+
+    const previewPhoto = photoURL => {
+        setPreviewPhotoURL(photoURL);
+        setIsPreviewShowing(true);
+    };
+
+    const onPreviewShow = () => setIsPreviewShowing(true);
+    const onPreviewClose = () => setIsPreviewShowing(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchGallery = async () => {
+            try {
+                const { data } = await NetworkServices.getGallery(id);
+                if (data.code === 200) {
+                    // Success
+                    setGallery(data.data.gallery);
+                }
+                setIsLoading(false);
+            } catch (error) {
+                // Could not fetch the gallery
+            }
+        };
+        fetchGallery();
+    }, [id]);
+
+    if (isLoading) return null;
+    return !gallery ? (
+        <Container
+            sx={{
+                display: "flex",
+                height: "100vh",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+            <Typography variant="h1" fontWeight="800">
+                404
+            </Typography>
+            <Typography variant="h4">Gallery not found</Typography>
+            <Button to="/" sx={{ mt: 2 }} variant="light" component={Link}>
+                Return Home
+            </Button>
+        </Container>
+    ) : (
+        <Container sx={{ pt: 6, pb: 12 }}>
+            <Box textAlign="center">
+                <Box>
+                    <Typography variant="h1" fontWeight={800} mb={4}>
+                        {gallery.title}
+                    </Typography>
+                    {gallery.description && (
+                        <Typography paragraph>{gallery.description}</Typography>
+                    )}
+                    <Typography variant="h5">
+                        {gallery.user.full_name}
+                    </Typography>
+                    <Typography
+                        component={Link}
+                        to={`/${gallery.user.username}`}
+                        variant="subtitle2"
+                    >
+                        @{gallery.user.username}
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        {gallery.photos.length} Photos
+                    </Typography>
+                </Box>
+
+                {isAuthenticated && user.username === gallery.user.username && (
+                    <PhotosUploader
+                        onPhotoPreview={photoURL => {
+                            previewPhoto(photoURL);
+                        }}
+                        galleryId={gallery.id}
+                    />
+                )}
+            </Box>
+
+            <Divider sx={{ my: 4 }} variant="middle" />
+
+            {gallery.photos.length === 0 ? (
+                <Box py={12} textAlign="center">
+                    <Typography color="light.main" paragraph>
+                        No photos on this gallery yet!
+                    </Typography>
+                </Box>
+            ) : (
+                <Grid container columnSpacing={2} rowSpacing={4}>
+                    {gallery.photos.map(photo => (
+                        <Grid key={photo.id} item xs={12} sm={6} md={4} lg={3}>
+                            <PhotoCard
+                                onClick={() =>
+                                    previewPhoto(photo.full_size_url)
+                                }
+                                url={photo.thumbnail_url}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
+
+            <Lightbox
+                open={isPreviewShowing}
+                photoURL={previewPhotoURL}
+                onOpen={onPreviewShow}
+                onClose={onPreviewClose}
+            />
+        </Container>
+    );
+};
 
 export default Gallery;
