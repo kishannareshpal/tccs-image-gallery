d0ed26d0302d1c8ebe8cf2ce041ba8d921b75126
Kishan Jadav <kishan_jadav@hotmail.com>
2021-09-30 09:06:03 +0100

    feat: show message when no galleries

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Container, Typography, Grid } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 import GalleryCard from "../components/GalleryCard";
 import NetworkServices from "../services/network.services";
+import useUser from "../hooks/useUser";
 
 const Home = () => {
+    const { user, isAuthenticated } = useUser();
     const [galleries, setGalleries] = useState([]);
 
     useEffect(() => {
@@ -26,21 +29,38 @@ const Home = () => {
             <Typography variant="h4" fontWeight={800} mb={4}>
                 Explore
             </Typography>
-
-            <Grid container columnSpacing={2} rowSpacing={4}>
-                {galleries.map(gallery => (
-                    <Grid key={gallery.id} item xs={12} sm={6} md={4} lg={3}>
-                        <GalleryCard
-                            galleryId={gallery.id}
-                            title={gallery.title}
-                            description={gallery.description}
-                            username={gallery.user.username}
-                            photosCount={gallery.photos_count}
-                            thumbnailURL={gallery.thumbnail_url}
-                        />
-                    </Grid>
-                ))}
-            </Grid>
+            {galleries.length === 0 ? (
+                <Box>
+                    <Typography color="light.main" paragraph>
+                        There are no galleries at all!{" "}
+                        {isAuthenticated
+                            ? "Be the first to create on through your profile."
+                            : "Register an account or login to add your own gallery!"}
+                    </Typography>
+                </Box>
+            ) : (
+                <Grid container columnSpacing={2} rowSpacing={4}>
+                    {galleries.map(gallery => (
+                        <Grid
+                            key={gallery.id}
+                            item
+                            xs={12}
+                            sm={6}
+                            md={4}
+                            lg={3}
+                        >
+                            <GalleryCard
+                                galleryId={gallery.id}
+                                title={gallery.title}
+                                description={gallery.description}
+                                username={gallery.user.username}
+                                photosCount={gallery.photos_count}
+                                thumbnailURL={gallery.thumbnail_url}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
         </Container>
     );
 };
